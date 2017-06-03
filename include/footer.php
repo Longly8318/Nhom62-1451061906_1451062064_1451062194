608291d41d750cb6757c7dc5f09f046abac9ff29
phungduclong <longpd42@wru.vn>
2017-06-03 16:01:30 +0700

    update source 3/6/2017

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+<embed id="Advertisement" 
+width="960" height="100" 
+flashvars="clickTARGET=_blank&clickTAG=#" 
+allowscriptaccess="always"
+wmode="transparent" quality="high" 
+name="Advertisement" style="admincp" 
+src="images/quangcao.swf" 
+type="application/x-shockwave-flash">
+Đồ án Website kinh doanh máy tính xách tay trực tuyến</br>
+Thực hiện: Phùng Đức Long-56TH2 - phunglong30101996@gmail.com - 09071789000
